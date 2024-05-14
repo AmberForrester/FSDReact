4b0b4021c0bb96d57b8588ae006e693633f2ac98
AmberForrester <DivineIntelligence3.0@gmail.com>
2024-05-14 09:38:45 -0400

    controlled components

--- day8/src/Main.js
+++ day8/src/Main.js
@@ -1,82 +1,27 @@
 import React from "react";
-import { useState } from "react";
-import { FaTrashAlt } from "react-icons/fa";
+import ItemList from "./ItemList";
 
-const Main = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "Bread",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Eggs",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Milk",
-    },
-    {
-      id: 4,
-      checked: false,
-      item: "Bananas",
-    },
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map(
-      (item) => item.id === id ? {...item,checked: !item.checked} :item 
-    );
-
-    setItems(listItems);
-
-    localStorage.setItem('groceryList', JSON.stringify(listItems));
-  };
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-
-    setItems(listItems);
-
-    localStorage.setItem('groceryList', JSON.stringify(listItems));
-
-    };
-  
+const Main = ({ items, handleDelete, handleCheck }) => {
 
   return (
+
     <main>
 
       {items.length ? (
-        <ul>
-                {items.map((item) => (
-                  <li className="item" key={item.id}>
 
-                    <input onChange={() => handleCheck(item.id)}
-                    type="checkbox" 
-                    checked={item.checked} />
+        <ItemList
+          items={items}
+          handleDelete={handleDelete}
+          handleCheck={handleCheck}
+        />
 
-                    <label style={item.checked ? { textDecoration: "line-through" } : null}
-                    onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
+      ) : (
 
-                    <FaTrashAlt onClick={() => handleDelete(item.id)}
-                    role="button" 
-                    tabIndex="0" />
+        <p style={{ marginTop: "2rem", fontWeight: "bold" }}>You have completed your list! </p>
+      )}
 
-                  </li>
-                ))}
-              </ul>
-            ) : (
-            <p style={{ 
-              fontWeight: "bold" }}
-              >You have completed your list! </p>
-          )}
-     
     </main>
   );
 };
-
 
 export default Main;
