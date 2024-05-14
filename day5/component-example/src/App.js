cde900736405c3e9f0161a62adaf14a77e2bd027
AmberForrester <DivineIntelligence3.0@gmail.com>
2024-05-14 11:28:29 -0400

    useEffect example done

--- day5/component-example/src/App.js
+++ day5/component-example/src/App.js
@@ -3,25 +3,38 @@
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import { useEffect, useState } from "react";
 
 function App() {
 
-    const nameChange = () => {
+    const [emotion, SetEmotion] = useState("Happy");
 
-      const name = ["Syed", "Suzanne", "Salah", "Suze"];
+      useEffect(() => {
 
-      const n = Math.floor(Math.random() * 4);
+        console.log(`My initial emotion is ${emotion}`);}, [emotion]
+    );
 
-      return name[n];
+    // const nameChange = () => {
 
-    };
+    //   const name = ["Syed", "Suzanne", "Salah", "Suze"];
+
+    //   const n = Math.floor(Math.random() * 4);
+
+    //   return name[n];
+
+    // };
 
   return (
     <div className="App">
+      <h1>Your current emotion is {emotion}</h1>
 
-          <Header/>
+      <button onClick={() => SetEmotion("Excited")}>Excited</button>
+
+      <button onClick={() => SetEmotion("Eager")}>Eager</button>
+
+          {/* <Header/>
           <Main/>
-          <Footer/>
+          <Footer/> */}
 
     </div>
   );
