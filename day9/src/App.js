2fb3ca38bb62e37ed43084da18b861f2a14c2a29
AmberForrester <DivineIntelligence3.0@gmail.com>
2024-05-17 21:12:02 -0400

    duplicate file cleanup

--- day9/src/App.js
+++ day9/src/App.js
@@ -1,19 +1,15 @@
 
 import './App.css';
 import Content from './Content';
-import {useState} from "react";
-import SearchJob from './SearchJob';
 
 
 
 function App() {
 
-  const [search, setSearch] = useState("");
 
 
   return (
     <div className="App">
-      <SearchJob search={search} setSearch={setSearch} />
       <Content/>
       
     </div>
