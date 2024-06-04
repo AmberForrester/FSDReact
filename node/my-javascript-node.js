28db219ddd4b3133dbf41e446e0d57ac1964e7e6
AmberForrester <DivineIntelligence3.0@gmail.com>
2024-06-04 11:02:11 -0400

    node commands

--- node/my-javascript-node.js
+++ node/my-javascript-node.js
@@ -10,6 +10,7 @@ console.log(global);
 // inside node, there is a common core module called: commonJS module
 
 const os = require('os');
+const path = require('path');
 
 console.log(os.type());
 console.log(os.version());
@@ -19,3 +20,12 @@ console.log(os.homedir());
 console.log(__dirname);
 
 console.log(__filename);
+
+// directly call the path
+console.log(path.dirname(__filename))
+
+console.log(path.extname(__filename))
+
+// no API calls in node
+console.log(path.parse(__filename))
+
