98f0620195e62140cd4e68bb7524d39d6d87b646
Amber Forrester <DivineIntelligence3.0@gmail.com>
2024-06-15 22:40:52 -0400

    Event Emitter Done

---
+++ npm/index.js
@@ -0,0 +1,17 @@
+const myEventLogs = require('./eventLog');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter{};
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('mylogs', (msg) => myEventLogs(msg));
+
+setTimeout(
+
+    ()=>{
+
+        myEmitter.emit('mylogs', 'log item emitted');
+    
+    }, 2000);
