c8c3e720b2181d5564366831548d2b24b8039b7e
Amber Forrester <DivineIntelligence3.0@gmail.com>
2024-06-23 11:26:43 -0400

    Third party Middleware cors created

--- middleware/server.js
+++ middleware/server.js
@@ -6,9 +6,40 @@ const express = require('express');
 const app = express()
 const {logger} = require('./myCustomMiddleware/eventLog');
 
+
+const cors = require('cors');
+
+
 //Custom Middleware
 app.use(logger);
 
+// Create a whitelist "allowed list" that is R/T calling URL's because we want to restrict where the request comes to server.
+const whiteList = ["https://www.google.com",  "http://127.0.0.1:3400", "http://127.0.0.1:5500"];
+// For the URL's mentioned about let the source communicate with the server. 
+
+
+const corsOptions = {
+
+   origin: (origin, callback) => {
+
+                  // -1 allows whitelist to pick up URL and allow it 
+      if (whiteList.indexOf(origin) !== -1 || !origin) {
+         
+         // if no error, then whatever URL coming in allow to be true
+         callback(null, true);
+      }
+
+      else {
+
+         callback(new Error ("Not allowed by CORS"));
+      }
+   },
+
+   optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+// What was done using const corsOptions: created an object with key:value, key:value. The origin = key, value = code created within the () => {}.
 
 
 
