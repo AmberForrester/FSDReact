a586bb98cca200be21c9bffa42dcaa852f0367c2
Amber Forrester <DivineIntelligence3.0@gmail.com>
2024-07-09 02:43:49 -0400

    Angular - custom components + directives, types, and interface done

--- angular/hello-world/src/app/component/housinglocation/housinglocation.component.ts
+++ angular/hello-world/src/app/component/housinglocation/housinglocation.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../../component/housing-location';
 
 @Component({
   selector: 'app-housinglocation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      housing location works!
-    </p>
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    </section>
   `,
   styleUrl: './housinglocation.component.css'
 })
 export class HousinglocationComponent {
+  @Input() housingLocation!:HousingLocation;
 
 }
